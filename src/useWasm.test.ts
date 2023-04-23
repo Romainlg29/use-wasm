616e91b226b299fbe4ab7ab1de9511d41e97eee5
Romain <legall.romain29470@gmail.com>
2023-04-23 12:40:43 +0200

    🔧 updated to async/await

--- src/useWasm.test.ts
+++ src/useWasm.test.ts
@@ -1,16 +1,44 @@
 // @ts-nocheck - no types for testing
 import { renderHook } from '@testing-library/react';
+import fetch from 'jest-fetch-mock';
 import { useWasm } from './useWasm';
 
-global.fetch = jest.fn();
+fetch.enableMocks();
 
 describe('useWasm', () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
+
+		fetch.mockResponse(async (request) => {
+			if (request.url.endsWith('compute.wasm')) {
+				return {
+					url: request.url,
+					status: 200,
+					ok: true,
+					body: 'test',
+				} as Response;
+			} else {
+				return {
+					url: request.url,
+					status: 404,
+					ok: false,
+					statusText: 'Not Found',
+				} as Response;
+			}
+		});
+	});
+
+	it('should throw an error if no path has been provided.', () => {
+		expect(() => renderHook(() => useWasm(''))).toThrowError('Path is required');
 	});
 
 	// @ts-expect-error - path is required
-	it('should throw an error if path is not correct.', () => {
-		expect(() => renderHook(() => useWasm())).toThrowError();
-	});
+	/*it('should throw an error if path is not correct.', async () => {
+
+		const {
+			result,
+		} = renderHook(() => useWasm('path.to.png'));
+
+		expect(result.error!.message).toBe('Failed to fetch the WASM file: Not Found');
+	});*/
 });
--- src/useWasm.ts
+++ src/useWasm.ts
@@ -3,21 +3,40 @@ import { useEffect, useState } from 'react';
 export interface useWasmReturn<T> {
 	fn: T;
 	isLoading: boolean;
+	module: WebAssembly.Module;
+	instance: WebAssembly.Instance;
 }
 
-export const useWasm = <T>(
-	path: string,
-	options?: {
-		streaming?: boolean;
-		fetchOptions?: RequestInit;
-	}
-) => {
+export interface useWasmOptions {
+	/**
+	 * Streaming instantiation
+	 * Make sure that the server returns the correct MIME type to enable this.
+	 * @default false
+	 */
+	streaming?: boolean;
+
+	/**
+	 * Fetch options
+	 * @default undefined
+	 */
+	fetchOptions?: RequestInit;
+}
+
+/**
+ * Hook to load a wasm file and use it.
+ * @param path Path to the wasm file
+ * @param options Options
+ * @returns The wasm exports
+ */
+export const useWasm = <T>(path: string, options?: useWasmOptions) => {
 	/**
 	 * Hook state
 	 */
 	const [wasm, setWasm] = useState<useWasmReturn<T>>({
 		isLoading: true,
 		fn: {} as T,
+		module: {} as WebAssembly.Module,
+		instance: {} as WebAssembly.Instance,
 	});
 
 	useEffect(() => {
@@ -38,64 +57,56 @@ export const useWasm = <T>(
 			isLoading: true,
 		}));
 
-		const fns: Promise<T> = new Promise((resolve) => {
-			(async () => {
+		(async () => {
+			/**
+			 * Fetch the wasm file
+			 */
+			const file = await fetch(path, {
+				signal: controller.signal,
+				...options?.fetchOptions,
+				headers: {
+					...options?.fetchOptions?.headers,
+					'Content-Type': 'application/wasm',
+				},
+			});
+
+			if (!file.ok) {
+				throw new Error(`Failed to fetch the WASM file: ${file.statusText}`);
+			}
+
+			/**
+			 * Instantiate the wasm file
+			 */
+			let wa: WebAssembly.WebAssemblyInstantiatedSource;
+
+			if (options?.streaming) {
 				/**
-				 * Fetch the wasm file
+				 * Streaming instantiation
 				 */
-				const file = await fetch(path, {
-					signal: controller.signal,
-					...options?.fetchOptions,
-					headers: {
-						...options?.fetchOptions?.headers,
-						'Content-Type': 'application/wasm',
-					},
-				});
-
-				if (!file.ok) {
-					throw new Error(`Failed to fetch the WASM file: ${file.statusText}`);
-				}
-
+				wa = await WebAssembly.instantiateStreaming(file);
+			} else {
 				/**
-				 * Instantiate the wasm file
+				 * Buffer instantiation
 				 */
-				let wa: WebAssembly.WebAssemblyInstantiatedSource;
+				const buffer = await file.arrayBuffer();
+				wa = await WebAssembly.instantiate(buffer);
+			}
 
-				if (options?.streaming) {
-					/**
-					 * Streaming instantiation
-					 */
-					wa = await WebAssembly.instantiateStreaming(file);
-				} else {
-					/**
-					 * Buffer instantiation
-					 */
-					const buffer = await file.arrayBuffer();
-					wa = await WebAssembly.instantiate(buffer);
-				}
+			/**
+			 * Force the type of the exports to T
+			 */
+			const exports: T = wa.instance.exports as T;
 
-				/**
-				 * Force the type of the exports to T
-				 */
-				const exports: T = wa.instance.exports as T;
-
-				/**
-				 * Resolve the promise
-				 * @param exports The exports of the wasm file
-				 */
-				resolve(exports);
-			})();
-		});
-
-		fns.then((exports: T) => {
 			/**
 			 * Set the exports as the function
 			 */
 			setWasm({
 				isLoading: false,
 				fn: exports,
+				module: wa.module,
+				instance: wa.instance,
 			});
-		});
+		})();
 
 		return () => {
 			/**
